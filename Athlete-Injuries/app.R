9f0c90de7341921b019f14fd9690ebb10fcc0d06
LukasEmge <lemge@college.harvard.edu>
2020-11-09 23:02:22 -0500

    Updated 11/9 at 11:02

--- Athlete-Injuries/app.R
+++ Athlete-Injuries/app.R
@@ -11,35 +11,84 @@ library(shiny)
 library(tidyverse)
 library(shinythemes)
 
-data <- read_rds("data.rds")
+combine_data <- read_rds("combine_data.rds")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    theme = shinytheme("cyborg"),
+    theme = shinytheme("superhero"),
     navbarPage(
         "NFL Draft Combine Results",
-        tabPanel("Testing Data",
-                 titlePanel("Distribution of Vertical Jumps \n40 Yard Dash Times"),
+        tabPanel("Testing Correlation",
+                 titlePanel("Do results in some tests \ncorrelate with other tests?"),
                  
                  # Sidebar with a slider input for number of bins 
                  sidebarLayout(
                      sidebarPanel(
-                         sliderInput("bins",
-                                     "Number of bins:",
-                                     min = 1,
-                                     max = 100,
-                                     value = 50)
+                       h4(strong("Select Tests")),
+                       selectInput("test1",
+                                     "Test 1",
+                                     c("40 Yard Dash" = "x40yd",
+                                       "Broad Jump" = "broad_jump",
+                                       "3 Cone Drill" = "x3cone",
+                                       "Shuttle" = "shuttle",
+                                       "Vertical" = "vertical")),
+                       selectInput("test2",
+                                   "Test 2",
+                                   c("40 Yard Dash" = "x40yd",
+                                     "Broad Jump" = "broad_jump",
+                                     "3 Cone Drill" = "x3cone",
+                                     "Shuttle" = "shuttle",
+                                     "Vertical" = "vertical"))
+                                       
                      ),
                      
-                     # Show a plot of the generated distribution
                      mainPanel(
-                         plotOutput("distPlot1"), 
-                         plotOutput("distPlot2"),
-                         plotOutput("distPlot3")
+                       plotOutput("distPlot1"),
                      )
+                     
+                   
                  ) 
                  
                  ),
+        tabPanel("Tests Affects",
+                 titlePanel("How do test results affect draft position?"),
+                 sidebarLayout(
+                   sidebarPanel(
+                     h4(strong("Select Position")),
+                     selectInput("position",
+                                 "Position",
+                                 c("Defensive End" = "DE",
+                                   "Defensive Tackle" = "DT",
+                                   "Inside Linebacker" = "ILB",
+                                   "Outside Linebacker" = "OLB",
+                                   "Strong Safety" = "SS",
+                                   "Free Safety" = "FS",
+                                   "Cornerback" = "CB",
+                                   "Quarterback" = "QB",
+                                   "Offensive Tackle" = "OT",
+                                   "Offensive Guard" = "OG",
+                                   "Fullback" = "FB",
+                                   "Tight End" = "TE",
+                                   "Wide Reciever" = "WR",
+                                   "Running Back" = "RB",
+                                   "Punter" = "P")),
+                     selectInput("test",
+                                 "Test",
+                                 c("40 Yard Dash" = "x40yd",
+                                   "Broad Jump" = "broad_jump",
+                                   "3 Cone Drill" = "x3cone",
+                                   "Shuttle" = "shuttle",
+                                   "Vertical" = "vertical",
+                                   "Bench Press" = "bench_reps"))),
+                   mainPanel(
+                     plotOutput("distPlot2"),
+                   )
+                   )
+                 
+                 
+                 
+                 ),
+        tabPanel("Offense"),
         tabPanel("About",
                  titlePanel("About"),
                  br(),
@@ -76,35 +125,110 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
-
+    
     output$distPlot1 <- renderPlot({
-      data %>% 
-          ggplot(aes(x = vertical, fill = pos)) + 
-            geom_histogram(bins = input$bins) +
-            labs(x = "Vertical Jump (Inches)", 
-                 y = "Number of Players", 
-                 title = "Distribution of Vertical Jumps")
+        combine_data %>% 
+        select(choice = !!input$test1, choicey = !!input$test2, pos) %>% 
+            ggplot(aes(x = choice, y = choicey, color = pos)) + 
+            geom_point() + 
+            labs(x = input$test1, 
+                 y = input$test2,
+                 title = paste("Relation of ", input$test1, " and ", input$test2))
         
     })
-
-    output$distPlot2 <- renderPlot({
-        data %>% 
-         ggplot(aes(x = x40yd, fill = pos)) + 
-            geom_histogram(bins = input$bins) + 
-            labs(x = "40 Yard Dash Time (Sec.)", 
-                 y = "Number of Players",
-                 title = "Distribution of 40 Yard Dashes")
     
+    output$distPlot2 <- renderPlot({
+      combine_data %>%
+        select(choice = !!input$position, pos, choicey = !!input$test, pick) %>% 
+        filter(pos == choice) %>% 
+        ggplot(aes(x = choicey, y = pick)) + 
+        geom_point() + 
+        labs(x = "input$test", 
+             y = "Draft Position",
+             title = paste("Relation of Draft Position to ", input$test))
+      
     })
     
     output$distPlot3 <- renderPlot({
-        data %>% 
-            ggplot(aes(x = x40yd, y = vertical, color = pos)) + 
-            geom_point() + 
-            labs(x = "40 Yard Dash Time (Sec.)", 
-                 y = "Vertical (Inches)",
-                 title = "Relation of Vertical to 40 Yard Dash")
-        
+      combine_data %>%
+        select(choice = !!input$position2, pos, choicey = !!input$testOff, pick) %>% 
+        filter(pos %in% choice) %>% 
+        ggplot(aes(x = choicey, y = pick)) + 
+        geom_point() + 
+        labs(x = "input$testOff", 
+             y = "Draft Position",
+             title = paste("Relation of Draft Position to ", input$testOff))
+      
+    })
+    
+    output$distPlot4 <- renderPlot({
+      combine_data %>% 
+        ggplot(aes(x = x40yd, y = shuttle, color = pos)) + 
+        geom_point() + 
+        labs(x = "40 Yard Dash Time (Sec.)", 
+             y = "Shuttle (Sec.)",
+             title = "Relation of Shuttle to 40 Yard Dash")
+      
+    })
+    
+    output$distPlot5 <- renderPlot({
+      combine_data %>% 
+        ggplot(aes(x = vertical, y = broad_jump, color = pos)) + 
+        geom_point() + 
+        labs(x = "Vertical (Inches)",
+             y = "Broad Jump (Inches)",
+             title = "Relation of Broad Jump to Vertical")
+      
+    })
+    
+    output$distPlot6 <- renderPlot({
+      combine_data %>% 
+        ggplot(aes(x = vertical, y = x3cone, color = pos)) + 
+        geom_point() + 
+        labs(x  = "Vertical (Inches)",
+             y = "3 Cone Drill Time (Sec.)",
+             title = "Relation of 3 Cone Drill to Vertical")
+      
+    })
+    
+    output$distPlot7 <- renderPlot({
+      combine_data %>% 
+        ggplot(aes(x = vertical, y = shuttle, color = pos)) + 
+        geom_point() + 
+        labs(x = "Vertical (Inches)",
+             y = "Shuttle (Sec.)",
+             title = "Relation of Shuttle to Vertical")
+      
+    })
+    
+    output$distPlot8 <- renderPlot({
+      combine_data %>% 
+        ggplot(aes(x = broad_jump, y = x3cone, color = pos)) + 
+        geom_point() + 
+        labs(x = "Broad Jump (Inches)", 
+             y = "3 Cone Drill (Sec.)",
+             title = "Relation of 3 Cone Drill to Broad Jump")
+      
+    })
+    
+    output$distPlot9 <- renderPlot({
+      combine_data %>% 
+        ggplot(aes(x = broad_jump, y = shuttle, color = pos)) + 
+        geom_point() + 
+        labs(x = "Broad Jump (Inches))", 
+             y = "Shuttle (Sec.)",
+             title = "Relation of Shuttle to Broad Jump")
+      
+    })
+    
+    output$distPlot10 <- renderPlot({
+      combine_data %>% 
+        ggplot(aes(x = x3cone, y = shuttle, color = pos)) + 
+        geom_point() + 
+        labs(x = "3 Cone Drill (Sec.)", 
+             y = "Shuttle (Sec.)",
+             title = "Relation of Shuttle to 3 Cone Drill")
+      
     })
     
 }
