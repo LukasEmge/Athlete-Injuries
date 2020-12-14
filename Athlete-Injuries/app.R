6255b927e9e2be95d1fd48a241f7a16a958b2f30
Lukas Emge <lemge@college.harvard.edu>
2020-12-14 13:28:36 -0500

    Almost Done

--- Athlete-Injuries/app.R
+++ Athlete-Injuries/app.R
@@ -78,10 +78,24 @@ ui <- fluidPage(
                      
                      mainPanel(
                        plotOutput("distPlot1")
-                     )
-                     
-                   
-                 ) 
+                     ),
+                    
+                 ),
+                 br(),
+                 p("For the 40 yard dash, three cone drill, and shuttle,
+                       lower times arw better. For the ertical and broad jump,
+                       higher/farther jumps are better. Therefore, when you
+                       plot a combination 40 yard dash, three cone drill, or 
+                       shuttle together, a positive correlation means they have
+                       a relationship in which doing better on one indicates doing 
+                       better on another. When you plot broad jump and vertical 
+                       together, a positive correlation again means they have a 
+                       relationship in which doing better on one indicates doing 
+                       better on another. However, when plotting one of 40 yard
+                       dash, shuttle, or three cone drill with one of broad jump 
+                       or vertical, a negative correlation will actually mean a 
+                       relationship in which doing better on one indicates doing 
+                       better on another."),
                  
                  ),
         tabPanel("Tests Effects",
@@ -124,16 +138,28 @@ ui <- fluidPage(
                                    "3 Cone Drill" = "x3cone",
                                    "Shuttle" = "shuttle",
                                    "Vertical" = "vertical",
-                                   "Bench Press" = "bench_reps"))),
+                                   "Bench Reps" = "bench_reps"))),
                    
                    # This slider allows you to select which event you want to 
                    # look at and analyze.
                    
                    mainPanel(
                      plotOutput("distPlot2")
-                   )
-                   )
-                 
+                   ),
+                   ),
+                 br(),
+                 p("This graph shows how much a specific test infleunces draft
+                   position. It is important to note that a lower draft position
+                   indicated a player getting drafted earlier and is thus better.
+                   For 40 yard dash, shuttle, and three cone drill, since
+                   lower times are better as well, a positive correlation means 
+                   that doing better on these tests improves draft position. For
+                   broad jump and vertical jump, since a higher/farther jump
+                   is better, a negative correlation indicates that doing
+                   better on these tests improves draft position. For bench 
+                   press, since more reps is better, a negative correlation 
+                   indicated that performing better on this test improves draft
+                   position."),
                  
                  
                  ),
@@ -214,7 +240,7 @@ ui <- fluidPage(
                                    "3 Cone Drill" = "x3cone",
                                    "Shuttle" = "shuttle",
                                    "Vertical" = "vertical",
-                                   "Bench Press" = "bench_reps"))),
+                                   "Bench Reps" = "bench_reps"))),
                    
                    # This slider allows you to select the event you want to
                    # analyze.
@@ -225,6 +251,7 @@ ui <- fluidPage(
                      )
                    )
                  ),
+        
         
         tabPanel("Model",
                  titlePanel("How do test results affect draft position?"),
@@ -262,12 +289,158 @@ ui <- fluidPage(
                    )
         ),
         
+        tabPanel("Understanding the Tests",
+                 titlePanel("How to contextualize the results"),
+                 br(),
+                 h4("Understanding the Tests"),
+                 p("On the plots that you find on the different tabs, you will
+                 see analysis related to different tests taken at the NFL 
+                 Combine. It is important to put these tests in proper context. 
+                 Below you will find descriptions of the tests that are 
+                 analyzed in this project."),
+                 br(),
+                 p("40 YARD DASH: This test evaluates the straight-line 
+                   speed of an individual. Prospects are timed in a 40 yard
+                   sprint, with what constitutes good and bad times varying by 
+                   position. Overall, 40 yard dash times fall between the range
+                   of about 4.20 seconds to about 5.3 seconds, with lower, and
+                   thus faster, times being considered better."),
+                 br(),
+                 p("BROAD JUMP: This test evaluates the explosiveness of an
+                   athlete by testing how far they can jump. Prospects start
+                   standing behind a line and jump as far as possible, and the
+                   distance they cover is reported in inches. The most explosive
+                   athletes can cover over 132 inches (11 feet), while less 
+                   explosive athletes may not reach 120 inches (10 feet). Again,
+                   what constitutes a good broad jump and a bad broad jump 
+                   varies by position"),
+                 br(),
+                 p("VERTICAL JUMP: This test evaluates the explosiveness of an
+                   athlete by testing how high they can jump. Prospects start
+                   standing in place and then jump as high as they can. The 
+                   height they jump is measured and reported in inches, with 
+                   very explosive athletes jumping in the upper 30 inch range.
+                   Again, what constitutes a good vertical jump and a bad
+                   vertical jump varies by position."),
+                 br(),
+                 p("SHUTTLE: This test evaluates an athletes ability to change
+                   direction. Prospects start in a three point stance, with one
+                   hand on the ground, and then sprint 5 yards to the right, touch 
+                   a line, sprint 10 yards to the left, touch another 
+                   line, and then sprint 5 yards to the right again, finsihing
+                   through the line they started on. Athletes may also be tested
+                   starting out sprinting to the left instead of the right. A 
+                   very agile athlete may run a shuttle in the 4.0 to 4.1 second
+                   range, while less agile athletes will be more towards the 4.5
+                   to 4.6 and above range. Again, what constitutes a good shuttle
+                   time and bad shuttle time varies by position."),
+                 br(),
+                 p("THREE CONE DRILL: This test evaluates an athletes ability to
+                   change direction. Prospects start in a three point stance, with
+                   one hand on the ground, and sprint around cones placed in an
+                   L shape. A very agile athlete may run the three cone drill
+                   in around 6.6 to 6.7 seconds, while less agile athletes will
+                   be in the mid to upper 7 second range. Again, what constitutes
+                   a good and bad three cone drill time varies by position."),
+                 br(),
+                 p("BENCH PRESS: This test evaluates the strength of an athlete.
+                   The prospect performs a bench press, doing as many reps as
+                   possible at 225 pounds. The strongest athletes may do over 30 
+                   reps, while less strong athletes may not reach double digits.
+                   Again, what constitutes a good bench press and a bad bench
+                   press varies by position.")
+        ),
+        
+        tabPanel("Understanding the Positions",
+                 titlePanel("How to understand what each position means"),
+                 br(),
+                 h4("Understanding the Positions"),
+                 p("In the different tabs, the data is usually broken down
+                   by the different positions an athlete plays. This is due to
+                   the fact that each position has different roles and, in turn,
+                   has different expectation for performance levels in each of
+                   the tests. To better understand what each position means and
+                   the implication of the position on the test results, see the
+                   descriptions below."),
+                 br(),
+                 p("QUARTERBACK: The quarterback position refers to the player
+                   who recieves the ball at the start of each play, either 
+                   handing it to a running back or throwing it to wide receivers.
+                   Therefore, the results of the tests are usually less important
+                   for quarterbacks, as their ability to throw a football is what
+                   is important. Due to this, you find high variation of athleticism
+                   and strength at the quarterback position, and usually these
+                   players have average test results for all tests."),
+                 br(),
+                 p("OFFENSIVE TACKLE and OFFENSIVE GUARD: These 
+                   positions make up the offensive line, responsible for blocking
+                   opposing players. These players are larger, usually over 300 
+                   pounds, therefore the speed and athleticism of these positions
+                   are usually lower, but their strength is higher. Therefor, 
+                   you can expect these players to make up some of the worst
+                   speed and explosiveness test results (40 yard dash, etc.()),
+                   but the best strength test results."),
+                 br(),
+                 p("WIDE RECEIVER: This position is responsible for running routes
+                   and catching passses from the quarterback. Therefore, these 
+                   players are usually very athletic and test at the top end of
+                   almost all of the tests, except their strength may be 
+                   average."),
+                 p("TIGHT END and FULLBACK: These positions have both the roles of 
+                 blocking andreceiving passes, depending on the situatioin. 
+                 Therefore, these players are larger and contain the strength 
+                 close to that of offensive linemen, but the speed, agility, and
+                 explosiveness close to that of the wide receiver. Therefore, 
+                   expect their test results across the board to be average."),
+                 br(),
+                 p("RUNNING BACK: This position is responsible for running the
+                   ball. Therefore, the players must be able to withstand many
+                   hits and are usually strong athletes, but also must have
+                   good athleticism and explosiveness. There testing results 
+                   are usually slightly worse than that of wide receivers, but
+                   better than tight ends."),
+                 br(),
+                 p("DEFENSIVE END: This position is responsible for rushing the
+                   quarterback and breaking through blocks of the offensive
+                   linemen. These players are usually large and very strong,
+                   but contain better athleticism than that of the offensive 
+                   linemen. Expect their test results to be on the lower to 
+                   average level."),
+                 br(),
+                 p("DEFENSIVE TACKLE: This position is resposnible for playing
+                   on the interior of the defensivle line, and therefore must
+                   be made up of very large players in order to stop the run. 
+                   These players have the athleticism and strength similar to
+                   that of an offensive linemen, so you can expect below average
+                   test results in all except bench press."),
+                 br(),
+                 p("OUTSIDE LINEBACKER and INSIDE LINEBACKER: These players have
+                   both the roles of stopping the run and the pass, and therefore
+                   must be very strong and muscular, similar to a running back, 
+                   but must also have the athleticism close to that of a wide
+                   receiver. You can expect their test results to be average
+                   to above average."),
+                 br(),
+                 p("SAFETY AND CORNERBACK: This position is responsible for
+                   covering the wide receivers. Due to this, these players
+                   must be the most athletic players on the field and therefor
+                   usually make up the fastest and most explosive test
+                   results."),
+                 br(),
+                 p("PUNTERS: This position is responsible for punting the ball, 
+                   and therefore the test results have little impact on them
+                   and are highly variable, but usually lower on athleticism
+                   and strength.")
+                 
+                 
+                 ),
+        
         
         tabPanel("About",
                  titlePanel("About"),
                  br(),
                  h1("NFL Combine Background"),
-                 p("Every year in February, the country's top college football players attend the
+                 p("Every year in February, the country's top college footballplayers attend the
                    NFL Combine, where players are tested in different events such as the
                    40 yard dash, broad jump, vertical jump, and more. NFL teams send scouts to
                    evaluate players, and in April the NFL Draft occurs, where NFL teams take
@@ -288,7 +461,7 @@ ui <- fluidPage(
                       1027 observations."),
                  h1("About Me"),
                  p("I am a junior at Harvard College. I am a chemistry concentrator with
-                 plans to enter the medical field. I play quartback for the,
+                 plans to enter the medical field. I play quartback for the
                    varsity football team, so this specific data is very 
                    intriguing."),
                  h5("The source code for this Shiny App can be found at my 
@@ -398,7 +571,7 @@ server <- function(input, output) {
         drop_na(bench_reps) %>% 
         drop_na(broad_jump) %>% 
         drop_na(x3cone) %>% 
-        drop_na(shuttle) %>% 
+        drop_na(shuttle) %>%  
         filter(team == input$team1,
                pos == input$position3) %>% 
         select(event = input$test4, pos, team) -> comparison_data
@@ -496,7 +669,7 @@ server <- function(input, output) {
      output$table <- render_gt({
        
       combine_data_stan <- combine_data %>% 
-         select(x40yd, vertical, bench_reps, broad_jump, x3cone, shuttle, team, pos, pick) %>% 
+        select(x40yd, vertical, bench_reps, broad_jump, x3cone, shuttle, team, pos, pick) %>% 
          filter(pos == input$position4)
       
       set.seed(5)
@@ -543,7 +716,7 @@ server <- function(input, output) {
      output$table3 <- render_gt({
        
        combine_data_stan <- combine_data %>% 
-         select(x40yd, vertical, bench_reps, broad_jump, x3cone, shuttle, team, pos, pick) %>% 
+         select(x40yd, vertical, bench_reps, broad_jump, x3cone, shuttle, Team, pos, pick) %>% 
          filter(pos == input$position4)
        
        set.seed(4)
@@ -573,7 +746,7 @@ server <- function(input, output) {
      output$table5 <- render_gt({
        
        combine_data_stan <- combine_data %>% 
-         select(x40yd, vertical, bench_reps, broad_jump, x3cone, shuttle, team, pos, pick) %>% 
+         select(x40yd, vertical, bench_reps, broad_jump, x3cone, shuttle, team, pos, pick) %>%  
          filter(pos == input$position4)
        
        set.seed(11)
@@ -588,7 +761,7 @@ server <- function(input, output) {
      output$table6 <- render_gt({
        
        combine_data_stan <- combine_data %>% 
-         select(x40yd, vertical, bench_reps, broad_jump, x3cone, shuttle, team, pos, pick) %>% 
+         select(x40yd, vertical, bench_reps, broad_jump, x3cone, shuttle, team, pos, pick) %>%  
          filter(pos == input$position4)
        
        set.seed(12)
